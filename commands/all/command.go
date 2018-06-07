8eee44531d55e8398613fe1f189eeb6c4832807d
Maxim Krasilnikov <mak.krasilnikov@gmail.com>
2018-06-08 00:37:34 +0300

    Fixed all up, drivers using global vars for table names

--- commands/all/command.go
+++ commands/all/command.go
@@ -7,30 +7,26 @@ import (
 	"gopkg.in/urfave/cli.v2"
 )
 
-var (
-	migrator driver.Interface
-	seeder   driver.Interface
-)
-
 // Command returns migration CLI command
 func Command() *cli.Command {
 	return &cli.Command{
 		Name:  "all",
 		Usage: "All command combine migration and seed command",
-		Before: func(ctx *cli.Context) (err error) {
-			migrator, err = driver.New(config.MigrateDriverConfig())
-			if err != nil {
-				return
-			}
-			seeder, err = driver.New(config.SeedDriverConfig())
-			return
-		},
 		Subcommands: cli.CommandsByName([]*cli.Command{
 			&cli.Command{
 				Name:  "up",
 				Usage: "Up db to latest migration version and to latest seed.",
 				Action: func(ctx *cli.Context) error {
-					err := commands.Up(ctx, migrator)
+					migrator, err := driver.New(config.MigrateDriverConfig())
+					if err != nil {
+						return err
+					}
+					err = commands.Up(ctx, migrator)
+					if err != nil {
+						return err
+					}
+
+					seeder, err := driver.New(config.SeedDriverConfig())
 					if err != nil {
 						return err
 					}
