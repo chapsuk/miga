099f4c1f1b4b8fe75a3018267d089b488d46a3aa
Maxim Krasilnikov <mak.krasilnikov@gmail.com>
2023-01-28 00:57:19 +0200

    Skip clickhouse replicated Redo test (#23)

--- tests/migrations/goose_clickhouse_replicated/00002_wallets.sql
+++ tests/migrations/goose_clickhouse_replicated/00002_wallets.sql
@@ -2,7 +2,7 @@
 CREATE TABLE wallets_replicated ON CLUSTER '{cluster}' (
     id      BIGINT,
     user_id BIGINT
-) engine=MergeTree() order by id;
+) engine=ReplicatedMergeTree() order by id;
 
 -- +goose Down
-DROP TABLE IF EXISTS wallets_replicated ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS wallets_replicated ON CLUSTER '{cluster}' SYNC;
